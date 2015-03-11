a771de4d16de718fa6d58165cded601ef4e5ab07
Marcin Ochman <marcinochman1993@wp.pl>
2015-03-12 00:20:01 +0100

    Zmodyfikowano plik requirements.txt, Django potrafi połączyć się z bazą z MySql

--- WheatherAnalyzer/settings.py
+++ WheatherAnalyzer/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'WheatherAnalyzer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'WheatherAnalyzer',
+        'USER': 'root',
+        'PASSWORD':'maroch1993',
+        'HOST':'',
+        'PORT':''
     }
 }
 
