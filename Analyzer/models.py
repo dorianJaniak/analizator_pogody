736eaad7a4521dd19dac7a7ad2e49f01e4349b17
Marcin Ochman <marcinochman1993@wp.pl>
2015-04-06 21:51:56 +0200

    Dodano diagramy

--- Analyzer/models.py
+++ Analyzer/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Stacja(models.Model):
+    id=models.IntegerField(primary_key=True)
+    nazwa=models.CharField(max_length=30)
+
+class RodzajPomiaru(models.Model):
+    id=models.IntegerField(primary_key=True)
+    nazwa=models.CharField(max_length=20)
+
+
+class DanePomiarowe(models.Model):
+    id=models.IntegerField(primary_key=True)
+    wartosc=models.IntegerField()
+    rodzaj_pomiaru=models.ForeignKey(RodzajPomiaru, null=True)
+    stacja=models.ForeignKey(Stacja, null=True)
+    data=models.DateField()
