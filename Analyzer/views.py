16076c69804effcee3fecd5c0f8b514abd7ff012
Aleksandra Grzelak <aleksandra.grzelak@pwr.wroc.pl>
2015-05-09 10:37:44 +0200

    wczytywanie danych z pliku do bazy (todo algorytm czytajacy z bazy)

--- Analyzer/views.py
+++ Analyzer/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, get_object_or_404, \
-    render_to_response
+    render_to_response, redirect
 from Analyzer.models import Jednostka, RodzajPomiaru,DanePomiarowe, \
     Stacja, Algorithm
-from Analyzer.forms import LoginForm
+from Analyzer.forms import LoginForm, LoadFilenameForm
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.views.generic import TemplateView, FormView, CreateView,\
     UpdateView, DeleteView
@@ -24,6 +24,8 @@ from django.contrib.auth import REDIRECT_FIELD_NAME, login, logout
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.debug import sensitive_post_parameters
+import csv
+from io import TextIOWrapper
 
 
 class LoginRequiredMixin(object):
@@ -111,8 +113,50 @@ class StationsDetailView(TemplateView):
 class ForecastView(TemplateView):
     template_name = "analyzer/forecast.html"
 
-class AuthorsView(LoginRequiredMixin,TemplateView):
+class AuthorsView(TemplateView):
     template_name='analyzer/authors.html'
+
+class LoadDataView(LoginRequiredMixin,FormView):
+    template_name ='analyzer/load_data.html'
+    form_class = LoadFilenameForm
+    success_url = reverse_lazy("authors")
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form':form})
+
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+
+        if form.is_valid():
+            f = TextIOWrapper(request.FILES['file'].file, encoding=request.encoding)
+            list = csv.reader(f)
+
+            for row in list:
+                # print(row)
+                _,created=DanePomiarowe.objects.get_or_create(
+                        wartosc=int(row[3]),
+                        rodzaj_pomiaru=get_object_or_404(RodzajPomiaru,nazwa=row[2]),
+                        stacja=get_object_or_404(Stacja,nazwa=row[0]),
+                        data=datetime.datetime.strptime( row[1], "%Y%m%d")
+                    )
+
+            return redirect(self.success_url)
+        else:
+            return render(request, self.template_name, {'form':form})
+
+    def filenames(self):
+        return self.filename
+
+
+
+    def get_context_data(self, **kwargs):
+        context=super(LoadDataView, self).get_context_data(self,**kwargs)
+        context['filename']=self.filename
+        print("context "+self.filename)
+        return context
+
 
 # TODO
 def dataview(request, *args, **kwargs ):
--- WheatherAnalyzer/urls.py
+++ WheatherAnalyzer/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from Analyzer.views import IndexView, StationsView, \
     StationsDetailView, ForecastView, AuthorsView, \
     dataview, data_station_view, \
-    LoginView, LogoutView
+    LoginView, LogoutView, LoadDataView
 
 # urlpatterns=patterns('',url(r'^$',views.index,name='index'))
 urlpatterns = [
@@ -14,6 +14,7 @@ urlpatterns = [
     url(r'^authors/$',AuthorsView.as_view(),name='authors'),
     url(r'^forecast/forecast.png$',dataview,name='image_forecast'),
     url(r'^station/(?P<station_id>[0-9]+)/current.png$',data_station_view,name='image_current'),
+    url(r'^load_data/$', LoadDataView.as_view() , name="load_data"),
     url(r'^login/$', LoginView.as_view(), name="login"),
     url(r'^logout/$', LogoutView.as_view(), name="logout"),
     url(r'^admin/', include(admin.site.urls)),
